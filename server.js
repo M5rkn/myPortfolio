4efd1dab7e377030159d85ade534cbae3b01df79
M5rkn <markoilynickiy@gmail.com>
2025-06-06 21:51:23 +0200

    fix: исправлена ошибка MongoDB buffermaxentries

--- server.js
+++ server.js
@@ -218,8 +218,6 @@ mongoose.connect(MONGODB_URI, {
     maxPoolSize: 10,
     serverSelectionTimeoutMS: 5000,
     socketTimeoutMS: 45000,
-    bufferCommands: false,
-    bufferMaxEntries: 0,
     retryWrites: true,
     retryReads: true
 })
